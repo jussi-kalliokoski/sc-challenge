8b66c8f300310d31e3713e70c1c07963ae7a01f7
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-11-06 20:23:04 +0200

    Add some documentation, see #1

--- js/playlists.js
+++ js/playlists.js
@@ -2,7 +2,19 @@ var Playlists = function () {
 
 var lists
 
+/**
+ * A singleton interface for manipulating playlists
+ *
+ * @class Playlists
+*/
+
 var Playlists = {
+/**
+ * Updates the playlists from the 'database'.
+ *
+ * @method Playlists
+ * @static
+*/
 	load: function () {
 		try {
 			lists = JSON.parse(localStorage.getItem('playlists'))
@@ -13,11 +25,26 @@ var Playlists = {
 
 		if (!lists) lists = []
 	},
-
+/**
+ * Saves the changes made to the lists to the 'database'.
+ *
+ * @method Playlists
+ * @static
+*/
 	save: function () {
 		localStorage.setItem('playlists', JSON.stringify(lists))
 	},
 
+/**
+ * Creates a new playlist.
+ *
+ * @method Playlists
+ * @static
+ *
+ * @arg {String} name The name of the new playlist.
+ *
+ * @return {Object} The playlist created.
+*/
 	create: function (name) {
 		if (Playlists.getByName(name)) {
 			throw Error('Playlist already exists!')
@@ -33,10 +60,28 @@ var Playlists = {
 		return lists[lists.length - 1]
 	},
 
+/**
+ * Removes a playlist from the list of playlists.
+ *
+ * @method Playlists
+ * @static
+ *
+ * @arg {Object} playlist The playlist to remove.
+*/
 	remove: function (playlist) {
 		lists.splice(lists.indexOf(playlist), 1)
 	},
 
+/**
+ * Gets a playlist by its name.
+ *
+ * @method Playlists
+ * @static
+ *
+ * @arg {String} name The name of the playlist.
+ *
+ * @return {Object} The playlist requested.
+*/
 	getByName: function (name) {
 		for (var i=0; i<lists.length; i++) {
 			if (lists[i].name === name) return lists[i]
@@ -45,6 +90,16 @@ var Playlists = {
 		return null
 	},
 
+/**
+ * Gets a playlist by its short name.
+ *
+ * @method Playlists
+ * @static
+ *
+ * @arg {String} sname The short name of the playlist.
+ *
+ * @return {Object} The playlist requested.
+*/
 	getByShortName: function (sname) {
 		for (var i=0; i<lists.length; i++) {
 			if (lists[i].shortname === sname) return lists[i]
@@ -53,6 +108,16 @@ var Playlists = {
 		return null
 	},
 
+/**
+ * Creates a short name for a playlist based on its name.
+ *
+ * @method Playlists
+ * @static
+ *
+ * @arg {String} name The name of the playlist.
+ *
+ * @return {String} The generated short name.
+*/
 	createShortName: function (name) {
 		var sname = name.toLowerCase().replace(/[^a-z]+/g, '-')
 
@@ -64,6 +129,14 @@ var Playlists = {
 		return sname
 	},
 
+/**
+ * Gets all playlists the user has.
+ *
+ * @method Playlists
+ * @static
+ *
+ * @return {Array} The list of playlists.
+*/
 	getAll: function () {
 		return lists.slice()
 	}
