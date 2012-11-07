38c93e9249f416768e5c409b24d3da92d2895c4b
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-11-07 03:06:13 +0200

    Merged test manage-view

--- tests/manage-view/index.rb
+++ tests/manage-view/index.rb
@@ -1,8 +1,32 @@
-require "spec/ruby"
-require "spec/runner"
+require "selenium-webdriver"
+require "rspec"
+include RSpec::Expectations
 
-# output T/F as Green/Red
-ENV['RSPEC_COLOR'] = 'true'
+describe "CreateAndDeletePlaylist" do
 
-require "create-playlist"
-require "delete-playlist"
+  before(:each) do
+    @driver = Selenium::WebDriver.for :firefox
+    @base_url = "http://cloudlist.avd.io"
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+  end
+  
+  after(:each) do
+    @driver.quit
+    @verification_errors.should == []
+  end
+  
+  it "test_create_and_delete_playlist" do
+    @driver.get(@base_url + "/")
+    @driver.find_element(:id, "new-playlist").click
+    a = @driver.switch_to.alert
+    a.send_keys "test playlist"
+    a.accept
+    sleep 1
+    @driver.get(@base_url + "/")
+    @driver.find_element(:css, "button.btn.btn-mini").click
+    a = @driver.switch_to.alert
+    a.accept
+    sleep 1
+  end
+end
